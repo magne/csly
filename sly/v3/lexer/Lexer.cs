6d2f1f11fbfe7c18db599df66a15af0763f733db
Magne Rasmussen <magne.rasmussen@gmail.com>
2019-12-06 00:06:50 +0100

    Moved v2 RegEx Lexer to v3.

---
+++ sly/v3/lexer/Lexer.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using sly.lexer;
+
+namespace sly.v3.lexer
+{
+    /// <summary>
+    ///     T is the token type
+    /// </summary>
+    /// <typeparam name="T">T is the enum Token type</typeparam>
+    public class Lexer<T> : ILexer<T> where T : struct
+    {
+        private readonly IList<TokenDefinition<T>> tokenDefinitions = new List<TokenDefinition<T>>();
+
+        public void AddDefinition(TokenDefinition<T> tokenDefinition)
+        {
+            tokenDefinitions.Add(tokenDefinition);
+        }
+
+
+        public LexerResult<T> Tokenize(string source)
+        {
+            List<Token<T>> tokens = new List<Token<T>>();
+            
+            var currentIndex = 0;
+            //List<Token<T>> tokens = new List<Token<T>>();
+            var currentLine = 1;
+            var currentColumn = 0;
+            var currentLineStartIndex = 0;
+            Token<T> previousToken = null;
+
+            while (currentIndex < source.Length)
+            {
+                currentColumn = currentIndex - currentLineStartIndex + 1;
+                TokenDefinition<T> matchedDefinition = null;
+                var matchLength = 0;
+
+                foreach (var rule in tokenDefinitions)
+                {
+                    var match = rule.Regex.Match(source.Substring(currentIndex));
+
+                    if (match.Success && match.Index == 0)
+                    {
+                        matchedDefinition = rule;
+                        matchLength = match.Length;
+                        break;
+                    }
+                }
+
+                if (matchedDefinition == null)
+                {
+                    return new LexerResult<T>(new LexicalError(currentLine, currentColumn, source[currentIndex]));
+                }
+
+                var value = source.Substring(currentIndex, matchLength);
+
+                if (matchedDefinition.IsEndOfLine)
+                {
+                    currentLineStartIndex = currentIndex + matchLength;
+                    currentLine++;
+                }
+
+                if (!matchedDefinition.IsIgnored)
+                {
+                    previousToken = new Token<T>(matchedDefinition.TokenID, value,
+                                                 new TokenPosition(currentIndex, currentLine, currentColumn));
+                    tokens.Add(previousToken);
+                }
+
+                currentIndex += matchLength;
+            }
+
+            var eos = new Token<T>();
+            eos.Position = new TokenPosition(previousToken.Position.Index + 1, previousToken.Position.Line,
+                                             previousToken.Position.Column + previousToken.Value.Length);
+
+
+            tokens.Add(eos);
+            return new LexerResult<T>(tokens);
+        }
+
+        public LexerResult<T> Tokenize(ReadOnlyMemory<char> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- sly/v3/lexer/LexerBuilder.cs
+++ sly/v3/lexer/LexerBuilder.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using sly.buildresult;
 using sly.lexer;
 using sly.lexer.fsm;
@@ -10,7 +13,63 @@ namespace sly.v3.lexer
             where TLexeme : struct
         {
             var attributes = sly.lexer.LexerBuilder.GetLexemes(result);
-            result = sly.lexer.LexerBuilder.Build(attributes, result, extensionBuilder);
+
+            if (IsRegexLexer(attributes) && !IsGenericLexer(attributes))
+            {
+                return BuildRegexLexer(attributes, result);
+            }
+
+            return sly.lexer.LexerBuilder.Build(attributes, result, extensionBuilder);
+        }
+
+        private static bool IsRegexLexer<TLexeme>(IDictionary<TLexeme, List<LexemeAttribute>> attributes)
+        {
+            return attributes.Values.SelectMany(list => list)
+                             .Any(lexeme => !string.IsNullOrEmpty(lexeme.Pattern));
+        }
+
+        private static bool IsGenericLexer<IN>(Dictionary<IN, List<LexemeAttribute>> attributes)
+        {
+            return attributes.Values.SelectMany(list => list)
+                             .Any(lexeme => lexeme.GenericToken != default);
+        }
+
+        private static BuildResult<ILexer<IN>> BuildRegexLexer<IN>(Dictionary<IN, List<LexemeAttribute>> attributes,
+                                                                   BuildResult<ILexer<IN>> result) where IN : struct
+        {
+            var lexer = new Lexer<IN>();
+            foreach (var pair in attributes)
+            {
+                var tokenID = pair.Key;
+
+                var lexemes = pair.Value;
+
+                if (lexemes != null)
+                {
+                    try
+                    {
+                        foreach (var lexeme in lexemes)
+                        {
+                            lexer.AddDefinition(new TokenDefinition<IN>(tokenID,
+                                                                        lexeme.Pattern,
+                                                                        lexeme.IsSkippable,
+                                                                        lexeme.IsLineEnding));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        result.AddError(new LexerInitializationError(ErrorLevel.ERROR,
+                                                                     $"error at lexem {tokenID} : {e.Message}"));
+                    }
+                }
+                else if (!tokenID.Equals(default(IN)))
+                {
+                    result.AddError(new LexerInitializationError(ErrorLevel.WARN,
+                                                                 $"token {tokenID} in lexer definition {typeof(IN).FullName} does not have Lexeme"));
+                }
+            }
+
+            result.Result = lexer;
             return result;
         }
     }
